0c9ae3395e213d354a4f2c4b256e3b63d7e72ef1
mmutopia <43702131+mmutopia@users.noreply.github.com>
2019-08-07 15:07:55 +0800

    Update more.sh

--- more.sh
+++ more.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 #参数是什么呢？？
+1、部署linux 介质步骤如下：
+   a. 用./web_update.sh iam_linux_xxxxxx.rar 
+   b. cd /iam_lux/bin    
+   c. source iam_env.sh
+   d. 执行 ./init_system.sh
 
+2、新机器第一次安装需要重启机器
